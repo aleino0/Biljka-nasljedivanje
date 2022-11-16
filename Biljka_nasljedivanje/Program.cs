80599e40d762a1473e6c6fdb5a4cbb2f533c9540
aleino0 <113008053+aleino0@users.noreply.github.com>
2022-11-16 08:49:06 +0100

    Add files via upload

---
+++ Biljka_nasljedivanje/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biljka_nasljedivanje
+{
+    class Biljka
+    {
+    }
+    class Cvijet : Biljka
+    {
+    }
+
+    class Stablo : Biljka
+    {
+        bool otpadajuListovi;
+        //svojstvo
+        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+
+        //konstruktor
+        public Stablo(bool otpadajuListovi1)
+        {
+            otpadajuListovi = otpadajuListovi1;
+        }
+    }
+    class Bjelogoricno : Stablo
+    {
+        bool _otpadajuListovi;
+        public Bjelogoricno(bool otpadajuListovi1) : base(otpadajuListovi1)
+        {
+            _otpadajuListovi = otpadajuListovi1;
+        }
+        public override string ToString()
+        {
+            return "Otpadaju listovi = " + _otpadajuListovi;
+        }
+    }
+    class Crnogoricno : Stablo
+    {
+        bool _otpadajuListovi;
+        
+        public Crnogoricno(bool otpadajuListovi1) : base(otpadajuListovi1)
+        {
+            _otpadajuListovi = otpadajuListovi1;
+        }
+
+        public override string ToString()
+        {
+            return "Otpadaju listovi = " + _otpadajuListovi;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crnogoricno bor = new Crnogoricno(false);
+            Bjelogoricno hrast = new Bjelogoricno(true);
+
+            Console.WriteLine("Hrast je: " + hrast.ToString());
+            Console.WriteLine("Bor je: " + bor.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
